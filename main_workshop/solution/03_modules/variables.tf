29d233031d0abd8e1780e81bbe917d66c6f671b8
kromerh <HeikoKromer@tiberius.home>
2024-01-15 10:21:22 +0100

    variables

---
+++ main_workshop/solution/03_modules/variables.tf
@@ -0,0 +1,10 @@
+variable "owner" {
+  type        = string
+  description = "The owner of the resources"
+  default     = "css"
+}
+
+variable "environment" {
+  type        = string
+  description = "The environment of the resources"
+}
