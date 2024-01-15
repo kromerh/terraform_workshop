b5f0728908a96f7e67f82ec2f852c79a20cbe126
kromerh <HeikoKromer@tiberius.home>
2024-01-15 09:21:08 +0100

    part 1

--- main_workshop/01_build/main.tf
+++ main_workshop/01_build/main.tf
@@ -13,19 +13,6 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_storage_account" "the-best-storage-account-ever" {
-  name                     = "sttfworkshophkr"
-  resource_group_name      = azurerm_resource_group.the-best-resource-group-ever.name
-  location                 = azurerm_resource_group.the-best-resource-group-ever.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    owner       = "heiko",
-    environment = "dev"
-  }
-}
-
 resource "azurerm_resource_group" "the-best-resource-group-ever" {
   name     = "rg-tf-workshop-hkr"
   location = "switzerlandnorth"
